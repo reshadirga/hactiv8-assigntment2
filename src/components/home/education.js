b2c020c7df073d9a52bbf47de89c0fceff0e6b9d
reshadirga <official.reshadirga@gmail.com>
2021-02-09 22:25:01 +0700

    Update file structure

---
+++ src/components/home/education.js
@@ -0,0 +1,48 @@
+import React from "react"
+
+class Education extends React.Component {
+    
+    render() {
+    
+    const listEducation = [
+        {
+            campus: "LoremIpsum Institute of Information Technology",
+            degree: "Bachelor of Tehnology",
+            date: "September 2010 - July 2013",
+            description: "12.34"
+        },
+
+        {
+            campus: "LoremIpsum Institute of Information Technology",
+            degree: "Master of Technology",
+            date: "January 2015 - December 2016",
+            description: "10.11"
+        },
+    ]
+
+    return (
+        <>
+            <div className="container-fluid">
+                <h2>Education</h2>
+                <br></br><br></br>
+                {listEducation.map((v) =>
+                    <div className="row">
+                        <div className="col-8">
+                            <h3>{v.campus}</h3>
+                            <h4>{v.degree}</h4>
+                            <br></br>
+                            <p>{v.description}</p>
+                            <br></br>
+                        </div>
+                        <div className="col-4">
+                            <p className="educationDate">{v.date}</p>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </>
+    )
+}
+}
+
+export default Education;
